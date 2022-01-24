03b246d834ae8dfd293173602de46d579032c0b2
jhnlynn <jl12183@nyu.edu>
2022-01-24 11:19:28 -0500

    new main starter

---
+++ cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"go-play/routers"
+	"log"
+	"os"
+)
+
+func LoadEnv() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+		os.Exit(1)
+	}
+}
+
+func main() {
+	LoadEnv()
+
+	//awsAccessKeyID := GetEnvWithKey("AWS_ACCESS_KEY_ID")
+	//fmt.Println("My access key ID is ", awsAccessKeyID)
+
+	router := routers.Routers()
+
+	_ = router.Run()
+}
